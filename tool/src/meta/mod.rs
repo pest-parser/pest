35a18e73fc8edab1c838d4d1458024fa6d1cb897
Dragos Tiselice <dragostiselice@gmail.com>
2016-08-06 10:30:03 -0700

    Removed grammar checker in favor of upcoming better macro support in Rust.

--- tool/src/meta/mod.rs
+++
@@ -1,8 +0,0 @@
-// pest. Elegant, efficient grammars
-// Copyright (C) 2016  Dragoș Tiselice
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-pub mod parser;
