a2afd1fbdc64a38bce49c22535c04214e1642b7c
Dragos Tiselice <dragostiselice@gmail.com>
2017-02-10 00:03:54 +0200

    Removed rest and added input.
    Removed tests, examples, and benchmarks. Added
    immutable input data structures.

---
+++ pest/src/inputs/mod.rs
@@ -0,0 +1,5 @@
+mod input;
+mod string_input;
+
+pub use self::input::Input;
+pub use self::string_input::StringInput;
--- pest/src/lib.rs
+++ pest/src/lib.rs
@@ -1,7 +1,10 @@
 extern crate futures;
 
+mod inputs;
 mod token;
 mod token_stream;
 
+pub use inputs::Input;
+pub use inputs::StringInput;
 pub use token::Token;
 pub use token_stream::TokenStream;
