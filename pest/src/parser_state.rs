3a2b93ebe5c578f05b5c065fec81ca880a4cde9b
Dragos Tiselice <dragostiselice@gmail.com>
2017-02-15 18:20:10 +0200

    Added atomics and attempts.

--- pest/src/parser_state.rs
+++ pest/src/parser_state.rs
@@ -12,15 +12,17 @@ enum TokenDestination {
 
 /// A `struct` which contains the complete state of a `Parser`.
 pub struct ParserState<'a, Rule> {
-    input:     &'a Input,
-    pos:       usize,
-    sender:    UnboundedSender<Token<Rule>>,
-    queue:     Vec<Token<Rule>>,
-    dest:      TokenDestination,
-    is_atomic: bool
+    input:       &'a Input,
+    pos:         usize,
+    sender:      UnboundedSender<Token<Rule>>,
+    queue:       Vec<Token<Rule>>,
+    dest:        TokenDestination,
+    is_atomic:   bool,
+    attempts:    Vec<Rule>,
+    attempt_pos: usize
 }
 
-impl<'a, Rule> ParserState<'a, Rule> {
+impl<'a, Rule: Clone + Ord> ParserState<'a, Rule> {
     /// Creates a new `ParserState` from an `Input` and an `UnboundedSender`.
     ///
     /// # Examples
@@ -39,12 +41,14 @@ impl<'a, Rule> ParserState<'a, Rule> {
     /// ```
     pub fn new(input: &'a Input, sender: UnboundedSender<Token<Rule>>) -> ParserState<'a, Rule> {
         ParserState {
-            input:     input,
-            pos:       0,
-            sender:    sender,
-            queue:     vec![],
-            dest:      TokenDestination::Stream,
-            is_atomic: false
+            input:       input,
+            pos:         0,
+            sender:      sender,
+            queue:       vec![],
+            dest:        TokenDestination::Stream,
+            is_atomic:   false,
+            attempts:    vec![],
+            attempt_pos: 0
         }
     }
 
@@ -272,5 +276,149 @@ impl<'a, Rule> ParserState<'a, Rule> {
         self.pos = initial_pos;
 
         result
+    }
+
+    /// Matches the current `rule` while toggling atomicity.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate futures;
+    /// # extern crate pest;
+    /// # use futures::sync::mpsc::unbounded;
+    /// # use pest::{ParserState, StringInput, Token};
+    /// # fn main() {
+    /// let (s, _) = unbounded::<Token<()>>();
+    /// let input = StringInput::new("");
+    /// let mut state = ParserState::new(&input, s);
+    ///
+    /// assert!(!state.is_atomic());
+    /// state.atomic(true, |state| {
+    ///     assert!(state.is_atomic());
+    ///     true
+    /// });
+    /// assert!(!state.is_atomic());
+    /// # }
+    /// ```
+    #[inline]
+    pub fn atomic<F>(&mut self, is_atomic: bool, rule: F) -> bool
+        where F: FnOnce(&mut ParserState<'a, Rule>) -> bool {
+
+        let should_toggle = self.is_atomic != is_atomic;
+
+        if should_toggle {
+            self.is_atomic = is_atomic;
+        }
+
+        let result = rule(self);
+
+        if should_toggle {
+            self.is_atomic = !is_atomic;
+        }
+
+        result
+    }
+
+    /// Returns the state's current atomicity.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate futures;
+    /// # extern crate pest;
+    /// # use futures::sync::mpsc::unbounded;
+    /// # use pest::{ParserState, StringInput, Token};
+    /// # fn main() {
+    /// let (s, _) = unbounded::<Token<()>>();
+    /// let input = StringInput::new("");
+    /// let mut state = ParserState::new(&input, s);
+    ///
+    /// assert!(!state.is_atomic());
+    /// # }
+    /// ```
+    #[inline]
+    pub fn is_atomic(&self) -> bool {
+        self.is_atomic
+    }
+
+    /// Keeps track of failed rule attempts. It should be called when a `Rule` fails at current
+    /// `pos`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate futures;
+    /// # extern crate pest;
+    /// # use futures::sync::mpsc::unbounded;
+    /// # use pest::{ParserState, StringInput, Token};
+    /// # fn main() {
+    /// #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+    /// enum Rule {
+    ///     a,
+    ///     b
+    /// }
+    ///
+    /// let (s, _) = unbounded::<Token<Rule>>();
+    /// let input = StringInput::new("a");
+    /// let mut state = ParserState::new(&input, s);
+    ///
+    /// state.track(Rule::a);
+    /// assert_eq!(state.attempts(), (vec![Rule::a], 0));
+    ///
+    /// state.match_string("a");
+    ///
+    /// state.track(Rule::b);
+    /// assert_eq!(state.attempts(), (vec![Rule::b], 1));
+    /// # }
+    /// ```
+    #[inline]
+    pub fn track(&mut self, rule: Rule) {
+        if self.is_atomic || self.dest == TokenDestination::Ignore {
+            return
+        }
+
+        if self.attempts.is_empty() {
+            self.attempts.push(rule);
+
+            self.attempt_pos = self.pos;
+        } else {
+            if self.pos == self.attempt_pos {
+                self.attempts.push(rule);
+            } else if self.pos > self.attempt_pos {
+                self.attempts.clear();
+                self.attempts.push(rule);
+
+                self.attempt_pos = self.pos;
+            }
+        }
+    }
+
+    /// Returns a `Vec` of all attempted `Rule`s at the deepest position where the parsing last
+    /// stopped. It only returns leaves from the rule tree.
+    ///
+    /// It is commonly used for error reporting and debugging grammars.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate futures;
+    /// # extern crate pest;
+    /// # use futures::sync::mpsc::unbounded;
+    /// # use pest::{ParserState, StringInput, Token};
+    /// # fn main() {
+    /// let (s, _) = unbounded::<Token<()>>();
+    /// let input = StringInput::new("");
+    /// let mut state = ParserState::new(&input, s);
+    ///
+    /// state.track(());
+    /// assert_eq!(state.attempts(), (vec![()], 0));
+    /// # }
+    /// ```
+    #[inline]
+    pub fn attempts(&mut self) -> (Vec<Rule>, usize) {
+        self.attempts.sort();
+        self.attempts.dedup();
+
+        (self.attempts.iter().cloned().collect(), self.attempt_pos)
     }
 }
