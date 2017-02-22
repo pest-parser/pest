1dd514d257b1154e03c25472ef7c125bc4fa27b1
Dragos Tiselice <dragostiselice@gmail.com>
2017-02-22 17:55:01 +0200

    Reverted rename.
    Reverted the rename of TokenStream and made
    its constructor flexible in terms of the Stream
    it is being made out of.

--- pest/src/parser.rs
+++ pest/src/parser.rs
@@ -1,8 +1,13 @@
+use futures::stream::Stream;
+
+use super::error::Error;
 use super::inputs::Input;
-use super::streams::parser_stream::ParserStream;
+use super::tokens::Token;
+use super::tokens::TokenStream;
 
 /// A `trait` that defines a `Parser`.
 pub trait Parser<Rule> {
     /// Parses `input` starting from `rule` and returns a stream of `Token`s.
-    fn parse(rule: Rule, input: &Input) -> ParserStream<Rule>;
+    fn parse<S>(rule: Rule, input: &Input) -> TokenStream<Rule, S>
+        where S: Stream<Item=Result<Token<Rule>, Error<Rule>>, Error=()>;
 }
--- pest/src/tokens/mod.rs
+++ pest/src/tokens/mod.rs
@@ -1,5 +1,7 @@
 mod token;
 mod token_data;
+mod token_stream;
 
 pub use self::token::Token;
 pub use self::token_data::TokenData;
+pub use self::token_stream::TokenStream;
