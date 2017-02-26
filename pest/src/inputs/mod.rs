45ba314f678762d9d09c727d76c93b4af6723638
Dragos Tiselice <dragostiselice@gmail.com>
2017-02-26 13:34:53 +0200

    Added docs to mods.

--- pest/src/inputs/mod.rs
+++ pest/src/inputs/mod.rs
@@ -1,3 +1,5 @@
+//! A `mod` containing the `Input` `trait` and implementations.
+
 mod input;
 mod string_input;
 
--- pest/src/tokens/mod.rs
+++ pest/src/tokens/mod.rs
@@ -1,7 +1,7 @@
+//! A `mod` containing token-related data structures.
+
 mod token;
 mod token_data;
-mod token_stream;
 
 pub use self::token::Token;
 pub use self::token_data::TokenData;
-pub use self::token_stream::TokenStream;
