97c059585946830ec24d7302d5721712f1bf1bbf
Dragos Tiselice <dragostiselice@gmail.com>
2017-03-08 12:27:04 +0200

    Made Input uses static.

--- pest/src/parser.rs
+++ pest/src/parser.rs
@@ -1,4 +1,5 @@
 use std::fmt::Debug;
+use std::sync::Arc;
 
 use super::inputs::Input;
 use super::streams_private::parser_stream::ParserStream;
@@ -6,5 +7,5 @@ use super::streams_private::parser_stream::ParserStream;
 /// A `trait` that defines a `Parser`.
 pub trait Parser<Rule: Debug + Eq + 'static> {
     /// Parses `input` starting from `rule` and returns a `ParserStream` of `Token`s.
-    fn parse<S>(rule: Rule, input: &Input) -> ParserStream<Rule>;
+    fn parse<I: Input>(rule: Rule, input: Arc<I>) -> ParserStream<Rule>;
 }
