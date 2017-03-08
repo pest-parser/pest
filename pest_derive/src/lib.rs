9f28389a75035aeb770d1075aaa9b78568e04156
Dragos Tiselice <dragostiselice@gmail.com>
2017-03-08 17:45:03 +0200

    Added copyright headers.

--- pest_derive/src/lib.rs
+++ pest_derive/src/lib.rs
@@ -1,3 +1,10 @@
+// pest. The Elegant Parser
+// Copyright (C) 2017  Dragoș Tiselice
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 extern crate proc_macro;
 #[macro_use]
 extern crate quote;
