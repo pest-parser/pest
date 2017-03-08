f0c06e6e1c7440c5994f12cb5cc049f229ffcc6a
Dragos Tiselice <dragostiselice@gmail.com>
2017-03-08 17:45:03 +0200

    Added copyright headers.

--- pest/src/streams_private/pair_stream.rs
+++ pest/src/streams_private/pair_stream.rs
@@ -1,4 +1,11 @@
 use std::cell::RefCell;
+// pest. The Elegant Parser
+// Copyright (C) 2017  Dragoș Tiselice
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use std::fmt::Debug;
 use std::rc::Rc;
 
