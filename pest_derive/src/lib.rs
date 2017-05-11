a6bf48a517acbfe418b4b581618a2b717996b044
Dragos Tiselice <dragostiselice@gmail.com>
2017-05-11 13:19:47 +0300

    Added html_root_url.

--- pest_derive/src/lib.rs
+++ pest_derive/src/lib.rs
@@ -5,6 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#![doc(html_root_url = "https://docs.rs/pest_derive/1.0.0")]
+
 extern crate proc_macro;
 #[macro_use]
 extern crate quote;
