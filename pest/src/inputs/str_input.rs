956f2be8bc1fc75d3f532a7a142586745c153fb3
Ashe Connor <ashe@kivikakk.ee>
2017-11-20 14:11:50 +1100

    Compile on non-nightly too
    Whoops: https://users.rust-lang.org/t/psa-dealing-with-warning-unused-import-std-ascii-asciiext-in-today-s-nightly/13726

--- pest/src/inputs/str_input.rs
+++ pest/src/inputs/str_input.rs
@@ -5,6 +5,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#[allow(unused_imports)] use std::ascii::AsciiExt;
 use std::ffi::OsString;
 use std::ops::Range;
 use std::str;
