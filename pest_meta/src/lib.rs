1f471b249cf6681b34f4b08a8474bf9147a852d4
Victor-Nicolae Savu <victor.nicolae.savu@gmail.com>
2018-01-27 19:11:20 +0100

    Qualify grammar errors

--- pest_meta/src/lib.rs
+++ pest_meta/src/lib.rs
@@ -14,7 +14,7 @@ pub mod validator;
 use std::fmt::Display;
 
 pub fn unwrap_or_report<T, E>(res: Result<T, E>) -> T where E: IntoIterator, E::Item: Display {
-    res.unwrap_or_else(|e| panic!(e.into_iter()
+    res.unwrap_or_else(|e| panic!("grammar error\n\n".to_owned() + &e.into_iter()
               .map(|error| { format!("{}", error) })
               .collect::<Vec<_>>()
               .join("\n\n")))
