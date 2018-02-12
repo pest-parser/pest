f804dda64a6e08d473b1eeebc10a24195da82b66
Dragos Tiselice <dragostiselice@gmail.com>
2018-02-12 14:50:14 +0100

    Fixed rotater.

--- derive/src/optimizer/rotater.rs
+++ derive/src/optimizer/rotater.rs
@@ -10,30 +10,36 @@
 use ast::*;
 
 pub fn rotate(rule: Rule) -> Rule {
+    fn rotate_internal(expr: Expr) -> Expr {
+        match expr {
+            // TODO: Use box syntax when it gets stabilized.
+            Expr::Seq(lhs, rhs) => {
+                let lhs = *lhs;
+                match lhs {
+                    Expr::Seq(ll, lr) => {
+                        rotate_internal(Expr::Seq(ll, Box::new(Expr::Seq(lr, rhs))))
+                    },
+                    lhs => Expr::Seq(Box::new(lhs), rhs)
+                }
+            }
+            Expr::Choice(lhs, rhs) => {
+                let lhs = *lhs;
+                match lhs {
+                    Expr::Choice(ll, lr) => {
+                        rotate_internal(Expr::Choice(ll, Box::new(Expr::Choice(lr, rhs))))
+                    }
+                    lhs => Expr::Choice(Box::new(lhs), rhs)
+                }
+            }
+            expr => expr
+        }
+    }
+
     match rule {
         Rule { name, ty, expr } => Rule {
             name,
             ty,
-            // TODO: Use box syntax when it gets stabilized.
-            expr: expr.map_bottom_up(|expr| match expr {
-                Expr::Seq(lhs, rhs) => {
-                    let lhs = *lhs;
-                    match lhs {
-                        Expr::Seq(ll, lr) => Expr::Seq(ll, Box::new(Expr::Seq(lr, rhs))),
-                        lhs => Expr::Seq(Box::new(lhs), rhs)
-                    }
-                }
-                Expr::Choice(lhs, rhs) => {
-                    let lhs = *lhs;
-                    match lhs {
-                        Expr::Choice(ll, lr) => {
-                            Expr::Choice(ll, Box::new(Expr::Choice(lr, rhs)))
-                        }
-                        lhs => Expr::Choice(Box::new(lhs), rhs)
-                    }
-                }
-                expr => expr
-            })
+            expr: expr.map_top_down(rotate_internal)
         }
     }
 }
