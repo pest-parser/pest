6be0b92c81b32f9b68db7f53e08e98a239e3bb01
Dragos Tiselice <dragostiselice@gmail.com>
2018-02-20 18:26:13 +0100

    Added skip to VM.

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -34,7 +34,7 @@ impl Vm {
     ) -> Option<Result<Pairs<'i, &str>, Error<'i, &str>>> {
         if let Some(rule) = self.rules.get(rule) {
             let result = pest::state(input, |mut state, pos| {
-                self.parse_rule(input, rule, pos, &mut state)
+                self.parse_rule(rule, pos, &mut state)
             });
 
             Some(result)
@@ -45,45 +45,114 @@ impl Vm {
 
     fn parse_rule<'a, 'i>(
         &self,
-        input: &'i str,
         rule: &'a Rule,
         pos: Position<'i>,
         state: &mut ParserState<'i, &'a str>
     ) -> Result<Position<'i>, Position<'i>> {
-        let is_atomic = (rule.ty == RuleType::Atomic || rule.ty == RuleType::CompoundAtomic)
-            || &rule.name == "whitespace" || &rule.name == "comment";
-
-        match rule.ty {
-            RuleType::Normal => state.rule(&rule.name, pos, |state, pos| {
-                self.parse_expr(input, &rule.expr, is_atomic, pos, state)
-            }),
-            RuleType::Silent => self.parse_expr(input, &rule.expr, is_atomic, pos, state),
-            RuleType::Atomic => state.rule(&rule.name, pos, |state, pos| {
-                state.atomic(Atomicity::Atomic, move |state| {
-                    self.parse_expr(input, &rule.expr, is_atomic, pos, state)
+        if &rule.name == "whitespace" || &rule.name == "comment" {
+            match rule.ty {
+                RuleType::Normal => state.rule(&rule.name, pos, |state, pos| {
+                    state.atomic(Atomicity::Atomic, move |state| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
+                }),
+                RuleType::Silent => state.atomic(Atomicity::Atomic, move |state| {
+                    self.parse_expr(&rule.expr, pos, state)
+                }),
+                RuleType::Atomic => state.rule(&rule.name, pos, |state, pos| {
+                    state.atomic(Atomicity::Atomic, move |state| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
+                }),
+                RuleType::CompoundAtomic => state.atomic(Atomicity::CompoundAtomic, move |state| {
+                    state.rule(&rule.name, pos, |state, pos| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
+                }),
+                RuleType::NonAtomic => state.atomic(Atomicity::Atomic, move |state| {
+                    state.rule(&rule.name, pos, |state, pos| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
                 })
-            }),
-            RuleType::CompoundAtomic => state.atomic(Atomicity::CompoundAtomic, move |state| {
-                state.rule(&rule.name, pos, |state, pos| {
-                    self.parse_expr(input, &rule.expr, is_atomic, pos, state)
+            }
+        } else {
+            match rule.ty {
+                RuleType::Normal => state.rule(&rule.name, pos, |state, pos| {
+                    self.parse_expr(&rule.expr, pos, state)
+                }),
+                RuleType::Silent => self.parse_expr(&rule.expr, pos, state),
+                RuleType::Atomic => state.rule(&rule.name, pos, |state, pos| {
+                    state.atomic(Atomicity::Atomic, move |state| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
+                }),
+                RuleType::CompoundAtomic => state.atomic(Atomicity::CompoundAtomic, move |state| {
+                    state.rule(&rule.name, pos, |state, pos| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
+                }),
+                RuleType::NonAtomic => state.atomic(Atomicity::NonAtomic, move |state| {
+                    state.rule(&rule.name, pos, |state, pos| {
+                        self.parse_expr(&rule.expr, pos, state)
+                    })
                 })
-            }),
-            RuleType::NonAtomic => state.atomic(Atomicity::NonAtomic, move |state| {
-                state.rule(&rule.name, pos, |state, pos| {
-                    self.parse_expr(input, &rule.expr, is_atomic, pos, state)
-                })
-            })
+            }
         }
     }
 
     fn parse_expr<'i>(
         &self,
-        input: &'i str,
         expr: &Expr,
-        is_atomic: bool,
-        pos: Position,
+        pos: Position<'i>,
         state: &mut ParserState<'i, &str>
     ) -> Result<Position<'i>, Position<'i>> {
         unimplemented!()
+    }
+
+    fn skip<'a, 'i>(
+        &'a self,
+        pos: Position<'i>,
+        state: &mut ParserState<'i, &'a str>
+    ) -> Result<Position<'i>, Position<'i>> {
+        match (self.rules.get("whitespace"), self.rules.get("comment")) {
+            (None, None) => Ok(pos),
+            (Some(whitespace), None) => if state.atomicity == Atomicity::NonAtomic {
+                pos.repeat(|pos| {
+                    self.parse_rule(whitespace, pos, state)
+                })
+            } else {
+                Ok(pos)
+            },
+            (None, Some(comment)) => if state.atomicity == Atomicity::NonAtomic {
+                pos.repeat(|pos| {
+                    self.parse_rule(comment, pos, state)
+                })
+            } else {
+                Ok(pos)
+            },
+            (Some(whitespace), Some(comment)) => if state.atomicity == Atomicity::NonAtomic {
+                state.sequence(move |state| {
+                    pos.sequence(|pos| {
+                        pos.repeat(|pos| {
+                            self.parse_rule(whitespace, pos, state)
+                        }).and_then(|pos| {
+                            pos.repeat(|pos| {
+                                state.sequence(move |state| {
+                                    pos.sequence(|pos| {
+                                        self.parse_rule(comment, pos, state).and_then(|pos| {
+                                            pos.repeat(|pos| {
+                                                self.parse_rule(whitespace, pos, state)
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            } else {
+                Ok(pos)
+            }
+        }
     }
 }
