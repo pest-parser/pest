6fa7fb76b5a033fd70e99e0dfebe4d9e02679cf8
Christopher Durham <cad97@cad97.com>
2018-06-21 21:56:45 -0400

    SHOUT_CASE unicode property rules

--- pest/src/unicode/mod.rs
+++ pest/src/unicode/mod.rs
@@ -8,13 +8,13 @@ macro_rules! char_property_functions {
     {$(
         mod $module:ident;
         [$(
-            ($func:ident, $trie:ident),
+            $prop:ident,
         )*];
     )*} => {$(
         #[allow(unused)]
         mod $module;
-        $(pub fn $func(c: char) -> bool {
-            self::$module::$trie.contains_char(c)
+        $(pub fn $prop(c: char) -> bool {
+            self::$module::$prop.contains_char(c)
         })*
     )*};
 }
@@ -22,65 +22,28 @@ macro_rules! char_property_functions {
 char_property_functions! {
     mod binary;
     [
-      (ASCII_Hex_Digit, ASCII_HEX_DIGIT), (Alphabetic, ALPHABETIC),
-      (Bidi_Control, BIDI_CONTROL), (Case_Ignorable, CASE_IGNORABLE),
-      (Cased, CASED), (Changes_When_Casefolded, CHANGES_WHEN_CASEFOLDED),
-      (Changes_When_Casemapped, CHANGES_WHEN_CASEMAPPED),
-      (Changes_When_Lowercased, CHANGES_WHEN_LOWERCASED),
-      (Changes_When_Titlecased, CHANGES_WHEN_TITLECASED),
-      (Changes_When_Uppercased, CHANGES_WHEN_UPPERCASED), (Dash, DASH),
-      (Default_Ignorable_Code_Point, DEFAULT_IGNORABLE_CODE_POINT),
-      (Deprecated, DEPRECATED), (Diacritic, DIACRITIC),
-      (Extender, EXTENDER), (Grapheme_Base, GRAPHEME_BASE),
-      (Grapheme_Extend, GRAPHEME_EXTEND), (Grapheme_Link, GRAPHEME_LINK),
-      (Hex_Digit, HEX_DIGIT), (Hyphen, HYPHEN),
-      (IDS_Binary_Operator, IDS_BINARY_OPERATOR),
-      (IDS_Trinary_Operator, IDS_TRINARY_OPERATOR),
-      (ID_Continue, ID_CONTINUE), (ID_Start, ID_START),
-      (Ideographic, IDEOGRAPHIC), (Join_Control, JOIN_CONTROL),
-      (Logical_Order_Exception, LOGICAL_ORDER_EXCEPTION),
-      (Lowercase, LOWERCASE), (Math, MATH),
-      (Noncharacter_Code_Point, NONCHARACTER_CODE_POINT),
-      (Other_Alphabetic, OTHER_ALPHABETIC),
-      (Other_Default_Ignorable_Code_Point, OTHER_DEFAULT_IGNORABLE_CODE_POINT),
-      (Other_Grapheme_Extend, OTHER_GRAPHEME_EXTEND),
-      (Other_ID_Continue, OTHER_ID_CONTINUE),
-      (Other_ID_Start, OTHER_ID_START), (Other_Lowercase, OTHER_LOWERCASE),
-      (Other_Math, OTHER_MATH), (Other_Uppercase, OTHER_UPPERCASE),
-      (Pattern_Syntax, PATTERN_SYNTAX),
-      (Pattern_White_Space, PATTERN_WHITE_SPACE),
-      (Prepended_Concatenation_Mark, PREPENDED_CONCATENATION_MARK),
-      (Quotation_Mark, QUOTATION_MARK), (Radical, RADICAL),
-      (Regional_Indicator, REGIONAL_INDICATOR),
-      (Sentence_Terminal, SENTENCE_TERMINAL), (Soft_Dotted, SOFT_DOTTED),
-      (Terminal_Punctuation, TERMINAL_PUNCTUATION),
-      (Unified_Ideograph, UNIFIED_IDEOGRAPH), (Uppercase, UPPERCASE),
-      (Variation_Selector, VARIATION_SELECTOR), (White_Space, WHITE_SPACE),
-      (XID_Continue, XID_CONTINUE), (XID_Start, XID_START),
+        // ASCII_HEX_DIGIT, // let this one be stripped out -- the full trie is wasteful for ASCII
+        ALPHABETIC, BIDI_CONTROL, CASE_IGNORABLE, CASED, CHANGES_WHEN_CASEFOLDED,
+        CHANGES_WHEN_CASEMAPPED, CHANGES_WHEN_LOWERCASED, CHANGES_WHEN_TITLECASED,
+        CHANGES_WHEN_UPPERCASED, DASH, DEFAULT_IGNORABLE_CODE_POINT, DEPRECATED, DIACRITIC,
+        EXTENDER, GRAPHEME_BASE, GRAPHEME_EXTEND, GRAPHEME_LINK, HEX_DIGIT, HYPHEN,
+        IDS_BINARY_OPERATOR, IDS_TRINARY_OPERATOR, ID_CONTINUE, ID_START, IDEOGRAPHIC, JOIN_CONTROL,
+        LOGICAL_ORDER_EXCEPTION, LOWERCASE, MATH, NONCHARACTER_CODE_POINT, OTHER_ALPHABETIC,
+        OTHER_DEFAULT_IGNORABLE_CODE_POINT, OTHER_GRAPHEME_EXTEND, OTHER_ID_CONTINUE,
+        OTHER_ID_START, OTHER_LOWERCASE, OTHER_MATH, OTHER_UPPERCASE, PATTERN_SYNTAX,
+        PATTERN_WHITE_SPACE, PREPENDED_CONCATENATION_MARK, QUOTATION_MARK, RADICAL,
+        REGIONAL_INDICATOR, SENTENCE_TERMINAL, SOFT_DOTTED, TERMINAL_PUNCTUATION, UNIFIED_IDEOGRAPH,
+        UPPERCASE, VARIATION_SELECTOR, WHITE_SPACE, XID_CONTINUE, XID_START,
     ];
     
     mod category;
     [
-      (Cased_Letter, CASED_LETTER), (Close_Punctuation, CLOSE_PUNCTUATION),
-      (Connector_Punctuation, CONNECTOR_PUNCTUATION), (Control, CONTROL),
-      (Currency_Symbol, CURRENCY_SYMBOL),
-      (Dash_Punctuation, DASH_PUNCTUATION), (Decimal_Number, DECIMAL_NUMBER),
-      (Enclosing_Mark, ENCLOSING_MARK),
-      (Final_Punctuation, FINAL_PUNCTUATION), (Format, FORMAT),
-      (Initial_Punctuation, INITIAL_PUNCTUATION), (Letter, LETTER),
-      (Letter_Number, LETTER_NUMBER), (Line_Separator, LINE_SEPARATOR),
-      (Lowercase_Letter, LOWERCASE_LETTER), (Mark, MARK),
-      (Math_Symbol, MATH_SYMBOL), (Modifier_Letter, MODIFIER_LETTER),
-      (Modifier_Symbol, MODIFIER_SYMBOL), (Nonspacing_Mark, NONSPACING_MARK),
-      (Number, NUMBER), (Open_Punctuation, OPEN_PUNCTUATION),
-      (Other, OTHER), (Other_Letter, OTHER_LETTER),
-      (Other_Number, OTHER_NUMBER), (Other_Punctuation, OTHER_PUNCTUATION),
-      (Other_Symbol, OTHER_SYMBOL),
-      (Paragraph_Separator, PARAGRAPH_SEPARATOR), (Private_Use, PRIVATE_USE),
-      (Punctuation, PUNCTUATION), (Separator, SEPARATOR),
-      (Space_Separator, SPACE_SEPARATOR), (Spacing_Mark, SPACING_MARK),
-      (Surrogate, SURROGATE), (Symbol, SYMBOL),
-      (Titlecase_Letter, TITLECASE_LETTER), (Unassigned, UNASSIGNED),
-      (Uppercase_Letter, UPPERCASE_LETTER),
+        CASED_LETTER, CLOSE_PUNCTUATION, CONNECTOR_PUNCTUATION, CONTROL, CURRENCY_SYMBOL,
+        DASH_PUNCTUATION, DECIMAL_NUMBER, ENCLOSING_MARK, FINAL_PUNCTUATION, FORMAT,
+        INITIAL_PUNCTUATION, LETTER, LETTER_NUMBER, LINE_SEPARATOR, LOWERCASE_LETTER, MARK,
+        MATH_SYMBOL, MODIFIER_LETTER, MODIFIER_SYMBOL, NONSPACING_MARK, NUMBER, OPEN_PUNCTUATION,
+        OTHER, OTHER_LETTER, OTHER_NUMBER, OTHER_PUNCTUATION, OTHER_SYMBOL, PARAGRAPH_SEPARATOR,
+        PRIVATE_USE, PUNCTUATION, SEPARATOR, SPACE_SEPARATOR, SPACING_MARK, SURROGATE, SYMBOL,
+        TITLECASE_LETTER, UNASSIGNED, UPPERCASE_LETTER,
     ];
 }
