a455f854ba3b09e59a1ec583507212ce0c0f79b8
Nadrieril Feneanar <Nadrieril@users.noreply.github.com>
2019-02-28 14:36:43 +0100

    Fix #374

--- meta/src/validator.rs
+++ meta/src/validator.rs
@@ -410,7 +410,7 @@ fn validate_whitespace_comment<'a, 'i: 'a>(rules: &'a [ParserRule<'i>]) -> Vec<E
     rules
         .iter()
         .filter_map(|rule| {
-            if rule.name == "whitespace" || rule.name == "comment" {
+            if rule.name == "WHITESPACE" || rule.name == "COMMENT" {
                 if is_non_failing(&rule.node.expr, &map, &mut vec![]) {
                     Some(Error::new_from_span(
                         ErrorVariant::CustomError {
@@ -608,12 +608,12 @@ mod tests {
 
  --> 1:16
   |
-1 | whitespace = { \"\" }
+1 | WHITESPACE = { \"\" }
   |                ^^
   |
-  = whitespace cannot fail and will repeat infinitely")]
+  = WHITESPACE cannot fail and will repeat infinitely")]
     fn non_failing_whitespace() {
-        let input = "whitespace = { \"\" }";
+        let input = "WHITESPACE = { \"\" }";
         unwrap_or_report(consume_rules(
             PestParser::parse(Rule::grammar_rules, input).unwrap(),
         ));
@@ -624,12 +624,12 @@ mod tests {
 
  --> 1:13
   |
-1 | comment = { soi }
+1 | COMMENT = { soi }
   |             ^-^
   |
-  = comment is non-progressing and will repeat infinitely")]
+  = COMMENT is non-progressing and will repeat infinitely")]
     fn non_progressing_comment() {
-        let input = "comment = { soi }";
+        let input = "COMMENT = { soi }";
         unwrap_or_report(consume_rules(
             PestParser::parse(Rule::grammar_rules, input).unwrap(),
         ));
