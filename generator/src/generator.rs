7cfe56fab4360b4188939d1eed4cf8b7bc7af2b1
Benjamin Brittain <bwb@google.com>
2020-03-13 11:06:28 -0400

    Makes include_str! use a relative path

--- generator/src/generator.rs
+++ generator/src/generator.rs
@@ -166,9 +166,14 @@ fn generate_builtin_rules() -> Vec<(&'static str, TokenStream)> {
 // Needed because Cargo doesn't watch for changes in grammars.
 fn generate_include(name: &Ident, path: &str) -> TokenStream {
     let const_name = Ident::new(&format!("_PEST_GRAMMAR_{}", name), Span::call_site());
+    // Need to make this relative to the current directory since the path to the file
+    // is derived from the CARGO_MANIFEST_DIR environment variable
+    let mut current_dir = std::env::current_dir().expect("Unable to get current directory");
+    current_dir.push(path);
+    let relative_path = current_dir.to_str().expect("path contains invalid unicode");
     quote! {
         #[allow(non_upper_case_globals)]
-        const #const_name: &'static str = include_str!(#path);
+        const #const_name: &'static str = include_str!(#relative_path);
     }
 }
 
