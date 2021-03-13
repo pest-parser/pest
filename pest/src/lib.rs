d8b738fa033412ca9e2dc7987fa07f9b00d106fc
Michael Färber <01mf02@gmail.com>
2021-03-13 20:45:27 +0100

    cargo fmt.

--- pest/src/lib.rs
+++ pest/src/lib.rs
@@ -7,7 +7,6 @@
 // license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
-
 #![cfg_attr(feature = "const_prec_climber", feature(const_fn))]
 
 //! # pest. The Elegant Parser
