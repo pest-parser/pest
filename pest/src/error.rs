6e0466ed65b8caaeb8e4fe6dec7dda3dc7a0e3b2
Joshua P <jwpjr567@gmail.com>
2022-04-12 22:03:07 +0000

    Make error line contents public

--- pest/src/error.rs
+++ pest/src/error.rs
@@ -33,7 +33,7 @@ pub struct Error<R> {
     /// Line/column within the input string
     pub line_col: LineColLocation,
     path: Option<String>,
-    line: String,
+    pub line: String,
     continued_line: Option<String>,
 }
 
