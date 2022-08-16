abb1972359a2c3a3840de6480f80f9ac7943d9fa
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2022-08-16 19:45:24 +0800

    fix: put a call limit on the json fuzzer (#696)

--- grammars/fuzz/fuzz_targets/json.rs
+++ grammars/fuzz/fuzz_targets/json.rs
@@ -1,13 +1,17 @@
 #![no_main]
 #[macro_use]
 extern crate libfuzzer_sys;
+extern crate pest;
 extern crate pest_grammars;
+
+use std::convert::TryInto;
 
 fuzz_target!(|data: &[u8]| {
     use pest_grammars::json;
     use pest_grammars::Parser;
 
     if let Ok(s) = std::str::from_utf8(data) {
+        pest::set_call_limit(Some(45_000usize.try_into().unwrap()));
         let _ = json::JsonParser::parse(json::Rule::json, s);
     }
 });
