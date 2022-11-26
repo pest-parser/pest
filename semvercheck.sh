40974ab3bbe6b224a2cb5633751e894b3977b3e9
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2022-11-27 00:08:15 +0800

    ci: add pest_debugger to semver checks (#741)
    Co-authored-by: Tomas Tauber <me@tomtau.be>

--- semvercheck.sh
+++ semvercheck.sh
@@ -7,21 +7,21 @@ cargo build --package pest_bootstrap
 cargo run --package pest_bootstrap
 
 # current
-for crate in "pest_derive" "pest_generator" "pest_grammars" "pest_meta" "pest" "pest_vm"; do
-    cargo +nightly rustdoc -p $crate -- $RUSTDOC_LATE_FLAGS
+for crate in "pest_derive" "pest_generator" "pest_grammars" "pest_meta" "pest" "pest_vm" "pest_debugger"; do
+    cargo +nightly-2022-11-20 rustdoc -p $crate -- $RUSTDOC_LATE_FLAGS
     mv target/doc/$crate.json /tmp/current-$crate.json
 done
 
-# the latest 2.1.x release
-export BASELINE_GIT_SHA="7dee2a7770daa213b45f88e6af730868f764927a"
+# the 2.5.0 release
+export BASELINE_GIT_SHA="8c602d832e625a0965701618626166e2ffbd94bb"
 # baseline
 git fetch origin
 git checkout "$BASELINE_GIT_SHA"
 cargo clean
 cargo build --package pest_bootstrap
 cargo run --package pest_bootstrap
-for crate in "pest_derive" "pest_generator" "pest_grammars" "pest_meta" "pest" "pest_vm"; do
-    cargo +nightly rustdoc -p $crate -- $RUSTDOC_LATE_FLAGS
+for crate in "pest_derive" "pest_generator" "pest_grammars" "pest_meta" "pest" "pest_vm" "pest_debugger"; do
+    cargo +nightly-2022-11-20 rustdoc -p $crate -- $RUSTDOC_LATE_FLAGS
     mv target/doc/$crate.json /tmp/baseline-$crate.json
     echo "Checking $crate"
     cargo semver-checks check-release --current /tmp/current-$crate.json --baseline /tmp/baseline-$crate.json
