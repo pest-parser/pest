d5ce990f175405e75766bf5b88e364952988f804
博奕 黄 <1853308@tongji.edu.cn>
2023-07-12 20:50:07 +0800

    Update wrapper.rs

--- pest/src/typed/wrapper.rs
+++ pest/src/typed/wrapper.rs
@@ -7,10 +7,30 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
+pub trait ConstWrapper<T: Sized> {
+    /// Wrapped content.
+    const CONTENT: T;
+}
+
+pub trait ConstUnwrap<T: Sized> {
+    /// Get wrapped content.
+    fn const_unwrap(&self) -> T;
+}
+
+impl<T: Sized, U: ConstWrapper<T>> ConstUnwrap<T> for U {
+    fn const_unwrap(&self) -> T {
+        Self::CONTENT
+    }
+}
+
 /// A wrapper for string as a generics argument.
 pub trait StringWrapper {
     /// Wrapped string.
     const CONTENT: &'static str;
+}
+
+impl<T: StringWrapper> ConstWrapper<&'static str> for T {
+    const CONTENT: &'static str = T::CONTENT;
 }
 
 /// An object containing a string.
