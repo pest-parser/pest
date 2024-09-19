0a1533d5c6ade063c372134ab4240842d633e923
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2024-09-19 21:04:59 +0800

    bump version and adjust CI guards (#1039)
    * fix color output in CI

--- pest/src/error.rs
+++ pest/src/error.rs
@@ -1169,8 +1169,8 @@ mod tests {
 
         assert_eq!(
             format!("{:?}", miette_error),
-            vec![
-                "  \u{1b}[31m×\u{1b}[0m Failure to parse at (2, 1)",
+            [
+                "  \u{1b}[31m×\u{1b}[0m Failure to parse at Pos((2, 1))",
                 "   ╭────",
                 " \u{1b}[2m1\u{1b}[0m │ def",
                 "   · \u{1b}[35;1m┬\u{1b}[0m",
