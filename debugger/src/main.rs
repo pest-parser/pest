e29380aa6c7d1ee352cb5cb9ce14f631afc563a0
Jonas Zaddach <jonas.zaddach@gmail.com>
2025-01-31 06:28:03 +0100

    Allow specification of several breakpoints from cmdline args

--- debugger/src/main.rs
+++ debugger/src/main.rs
@@ -222,7 +222,7 @@ struct CliArgs {
     grammar_file: Option<PathBuf>,
     input_file: Option<PathBuf>,
     rule: Option<String>,
-    breakpoint: Option<String>,
+    breakpoints: Vec<String>,
     session_file: Option<PathBuf>,
     no_update: bool,
 }
@@ -233,7 +233,7 @@ impl Default for CliArgs {
             grammar_file: None,
             input_file: None,
             rule: None,
-            breakpoint: None,
+            breakpoints: Vec::new(),
             session_file: None,
             no_update: false,
         };
@@ -268,7 +268,7 @@ impl Default for CliArgs {
                 }
                 "-b" | "--breakpoint" => {
                     if let Some(breakpoint) = iter.next() {
-                        result.breakpoint = Some(breakpoint);
+                        result.breakpoints.push(breakpoint);
                     } else {
                         eprintln!("Error: missing breakpoint");
                         std::process::exit(1);
@@ -326,8 +326,8 @@ impl CliArgs {
                 eprintln!("Error: {}", e);
             }
         }
-        if let Some(breakpoint) = &self.breakpoint {
-            context.breakpoint(breakpoint);
+        for breakpoint in self.breakpoints {
+            context.breakpoint(& breakpoint);
         }
         if let Some(rule) = self.rule {
             if let Err(e) = context.run(&rule) {
