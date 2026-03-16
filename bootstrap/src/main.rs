bde65cde9e77cae2c6c8ae561aad128624b0f3fb
konstin <konstin@mailbox.org>
2026-03-16 14:00:12 +0100

    Keep `grammar.rs` in tree
    Fixes https://github.com/pest-parser/pest/issues/1158, see discussion over there.
    
    I tried formatting `grammar.rs` but Rustfmt doesn't finish formatting in a reasonable time.

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -3,12 +3,7 @@ extern crate quote;
 extern crate pest_generator;
 
 use pest_generator::derive_parser;
-use std::{
-    env,
-    fs::File,
-    io::prelude::*,
-    path::{Path, PathBuf},
-};
+use std::{fs::File, io::prelude::*, path::Path};
 
 fn main() {
     let pest = Path::new(concat!(
@@ -24,44 +19,20 @@ fn main() {
         .unwrap_or_else(|| &pest_ref);
     let pest = Path::new(&normalized_path);
 
-    // Path on which we should write generated grammar file.
-    // In case `not-bootstrap-in-src` is:
-    // * OFF -> in `grammar.rs` next to `grammar.pest`
-    // * ON  -> in `target/build/.../__pest_grammar.rs` directory as specified in `meta/build.rs`
-    let rs: PathBuf = if should_bootstrap_in_src() {
-        Path::new(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/../meta/src/grammar.rs"
-        ))
-        .to_owned()
-    } else {
-        // the path is passed via command-line arguments
-        let path = env::args().nth(1).expect("path to grammar.rs");
-        PathBuf::from(path)
-    };
-
     let derived = {
         let path = pest.to_string_lossy();
         let pest = quote! {
             #[grammar = #path]
             pub struct PestParser;
         };
-        // Passing value to `derive_parser` as if there was a derive annotation
-        // next to the struct above.
         derive_parser(pest, false)
     };
 
+    let rs = Path::new(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../meta/src/grammar.rs"
+    ));
     let mut file = File::create(rs).unwrap();
 
-    writeln!(file, "pub struct PestParser;\n{}", derived,).unwrap();
-}
-
-#[cfg(not(feature = "not-bootstrap-in-src"))]
-fn should_bootstrap_in_src() -> bool {
-    true
-}
-
-#[cfg(feature = "not-bootstrap-in-src")]
-fn should_bootstrap_in_src() -> bool {
-    false
+    writeln!(file, "pub struct PestParser;\n{}", derived).unwrap();
 }
